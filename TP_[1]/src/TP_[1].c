3536fea1e8377fd453315601d7b2327649c58518
supercandela <supercandela@gmail.com>
2022-04-10 14:16:16 -0300

    inicializo el repo

---
+++ TP_[1]/src/TP_[1].c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : TP_[1].c
+ Author      : Candela Bogado
+ División    : 1 "E"
+ Copyright   : Your copyright notice
+ Description : Trabajo Práctico 1 - Laboratorio 1
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
