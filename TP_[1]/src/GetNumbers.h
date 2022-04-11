7d48af06024a111dc7076d00d0b1a41d03cb53e7
supercandela <supercandela@gmail.com>
2022-04-10 22:18:57 -0300

    menu principal y algunas funciones

---
+++ TP_[1]/src/GetNumbers.h
@@ -0,0 +1,13 @@
+/*
+ * GetNumbers.h
+ *
+ *  Author: Candela Bogado
+ *  Created on: 10 abr. 2022
+ */
+
+#ifndef GETNUMBERS_H_
+#define GETNUMBERS_H_
+
+int getInt(int *pNumber, int minimumValue, int maximumValue, int tries);
+
+#endif /* GETNUMBERS_H_ */
